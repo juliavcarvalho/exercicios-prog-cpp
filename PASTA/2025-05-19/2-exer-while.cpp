9024886cf591866f7f6738a452e0cd326105906b
juliavcarvalho <jvmouracarvalho02@gmail.com>
2025-05-27 16:29:14 -0300

    usando while

---
+++ PASTA/2025-05-19/2-exer-while.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int contador = 1;
+    
+    while (contador <= 20) {
+        cout << "Valor do contador: " << contador << endl;
+        contador++;
+    }
+
+    return 0;
+}
